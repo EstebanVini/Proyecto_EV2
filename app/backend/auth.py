6e59a63d343d14f51fca83e9efc406dd09e4608a
Esteban Viniegra <0235320@up.edu.mx>
2024-06-06 21:13:39 -0600

    Update auth.py

--- app/backend/auth.py
+++ app/backend/auth.py
@@ -38,7 +38,3 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return token_data
 
-def get_current_active_user(current_user: TokenData = Depends(get_current_user)):
-    if current_user is None:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
