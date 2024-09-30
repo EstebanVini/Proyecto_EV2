471f29a322ca4c755cb0006f8671085bfba48182
Esteban Viniegra <0235320@up.edu.mx>
2024-09-30 17:46:48 -0600

    adding webapp

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.authRoute import routerAuth
 from app.routes.relationRoute import routerRelation
 from app.routes.messageRoute import routerMessage
 from app.routes.moviesRoute import routerMovie
 
 app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+app.mount("/", StaticFiles(directory="app/frontend/web", html=True), name="static")
+@app.get("/")
+def serve_frontend():
+    file_path = os.path.join("app/frontend/web", "index.html")
+    return FileResponse(file_path)
+
 
 app.include_router(routerAuth)
 app.include_router(routerRelation)
