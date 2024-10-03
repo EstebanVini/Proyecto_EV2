7509ef47dc33b3c6cf033e6b39f50c713ef52ea5
Esteban Viniegra <0235320@up.edu.mx>
2024-10-03 14:25:38 -0600

    Update for frontend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes.authRoute import routerAuth
 from app.routes.relationRoute import routerRelation
@@ -10,11 +13,17 @@ app = FastAPI()
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://proyectoe.eviniegra.software"], 
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
+
+app.mount("/", StaticFiles(directory="app/frontend/web", html=True), name="static")
+@app.get("/")
+def serve_frontend():
+    file_path = os.path.join("app/frontend/web", "index.html")
+    return FileResponse(file_path)
 
 app.include_router(routerAuth)
 app.include_router(routerRelation)
